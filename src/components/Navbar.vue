39348b3f9e64bb156e536ab3402a734e12b9b257
RobertLikesCoding <rbaufeld@posteo.de>
2026-02-27 10:00:46 +0100

    create desktop version

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="flex justify-center items-center gap-10">
+    <img
+      src="/src/assets/logo_menschlich.svg"
+      alt="Logo Ergotherapie Menschlich."
+    />
+    <ul class="flex gap-10">
+      <li class="hover:text-shadow-2xs">
+        <a href="#leistungen">Leistungen</a>
+      </li>
+      <li class="hover:text-shadow-2xs"><a href="#über">Über uns</a></li>
+      <li class="hover:text-shadow-2xs"><a href="#team">Team</a></li>
+      <li class="hover:text-shadow-2xs"><a href="#kontakt">Kontakt</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
