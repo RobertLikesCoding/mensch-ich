5cfaf35cf34b577ff5f9b55dce623d5fa72c6f0e
RobertLikesCoding <rbaufeld@posteo.de>
2026-03-01 12:36:50 +0100

    make hero work on desktop

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,14 +1,35 @@
 <template>
-  <a :href="to">
+  <a :href="to" :class="classes">
     {{ label }}
   </a>
 </template>
 
 <script setup lang="ts">
-  const props = defineProps<{
-    to: string;
-    label: string;
-    color?: "primary" | "secondary";
-    variant?: "filled" | "hollow";
-  }>();
+  import { computed } from "vue";
+
+  const props = withDefaults(
+    defineProps<{
+      to: string;
+      label: string;
+      color?: "primary" | "secondary";
+      variant?: "hollow";
+    }>(),
+    {
+      color: "secondary",
+    },
+  );
+
+  const classes = computed(() => {
+    const base = "rounded-full shadow px-3 py-2";
+    const background =
+      props.variant === "hollow" ? "" : `bg-${props.color}-500`;
+    const border =
+      props.variant === "hollow" ? `border-2 border-${props.color}-500` : "";
+    const text =
+      props.variant === "hollow"
+        ? `text-${props.color}-500`
+        : "text-neutral-100";
+
+    return [base, background, border, text].join(" ");
+  });
 </script>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,29 +3,33 @@
 </script>
 
 <template>
-  <section class="flex justify-center gradient h-dvh">
-    <div class="flex flex-col items-start gap-3">
+  <section
+    class="flex justify-center items-center gradient h-dvh overflow-hidden relative"
+  >
+    <div class="flex flex-col items-start gap-8 z-10">
       <h1 class="text-7xl leading-tight mb-5 handwritten">
-        Willkommen bei Menschlich
+        Willkommen bei Mensch/ich
       </h1>
       <span class="text-2xl">
         Ein Ort der Gesundheit, Entwicklung und Menschlichkeit in Berlin
         Adlershof.
       </span>
-      <p>
-        Genau hier setzen wir an. Wir begleiten Sie dabei, Ihre
-        Selbstständigkeit zurückzugewinnen und Ihre Gesundheit aktiv
-        mitzugestalten.
-      </p>
-      <p>
-        Sie kennen es vielleicht: Eine Verletzung, eine Erkrankung – und
-        plötzlich ist der Alltag nicht mehr der gleiche. Neben körperlichen
-        Beschwerden kommen Unsicherheiten:
-      </p>
-      <p>Was darf ich tun? Wie kann ich meine Genesung unterstützen?</p>
+      <div class="flex flex-col gap-3">
+        <p>
+          Genau hier setzen wir an. Wir begleiten Sie dabei, Ihre
+          Selbstständigkeit zurückzugewinnen und Ihre Gesundheit aktiv
+          mitzugestalten.
+        </p>
+        <p>
+          Sie kennen es vielleicht: Eine Verletzung, eine Erkrankung – und
+          plötzlich ist der Alltag nicht mehr der gleiche. Neben körperlichen
+          Beschwerden kommen Unsicherheiten:
+        </p>
+        <p>Was darf ich tun? Wie kann ich meine Genesung unterstützen?</p>
+      </div>
 
       <div class="flex gap-3">
-        <Button label="Termin vereinbaren" to="#kontakt" color="secondary" />
+        <Button label="Termin vereinbaren" to="#kontakt" />
         <Button
           label="Unsere Leistungen"
           to="#kontakt"
@@ -34,6 +38,13 @@
         />
       </div>
     </div>
+
+    <img
+      src="/src/assets/hero_logo.png"
+      alt="logo menschlich"
+      aria-hidden
+      class="absolute z-0 right-20 top-20"
+    />
   </section>
 </template>
 
