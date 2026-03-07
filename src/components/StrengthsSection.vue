ac2bb4ab13eaffb34a1212cd7285d6835eb8ea78
RobertLikesCoding <rbaufeld@posteo.de>
2026-03-07 22:54:08 +0100

    adjust strength layout

--- src/components/StrengthsSection.vue
+++ src/components/StrengthsSection.vue
@@ -1,35 +1,39 @@
 <template>
-  <section class="flex flex-col lg:items-center relative px-3 gap-5">
-    <h3 class="text-3xl font-bold text-neutral-100 mb-3">
-      Was uns auszeichnet
-    </h3>
-    <div class="flex gap-10">
-      <div class="text-center text-neutral-100">
-        <h4 class="text-2xl font-bold mb-3">Ganzheitlicher Blick</h4>
-        <p>
-          Wir betrachten körperliche, psychische und soziale Faktoren in ihrer
-          Wechselwirkung
-        </p>
-      </div>
-      <div class="text-center text-neutral-100">
-        <h4 class="text-2xl font-bold mb-3">Individuelle Beratung</h4>
-        <p>
-          Strukturierte, klientenzentrierte Behandlungskonzepte mit
-          transparenten Therapiezielen
-        </p>
-      </div>
-      <div class="text-center text-neutral-100">
-        <h4 class="text-2xl font-bold mb-3">Prävention & Coaching</h4>
-        <p>
-          Ergonomieberatung, Stressmanagement und betriebliche
-          Gesundheitsförderung
-        </p>
+  <section class="flex lg:justify-center px-3 gap-5 relative">
+    <div class="content-box flex flex-col">
+      <h3
+        class="text-3xl font-bold text-neutral-100 text-center mb-5 text-shadow-lg"
+      >
+        Was uns auszeichnet
+      </h3 text-shadow-lg>
+      <div class="flex gap-10">
+        <div class="text-center text-neutral-100 text-shadow-lg">
+          <h4 class="text-2xl font-bold mb-3">Ganzheitlicher Blick</h4>
+          <p>
+            Wir betrachten körperliche, psychische und soziale Faktoren in ihrer
+            Wechselwirkung
+          </p>
+        </div>
+        <div class="text-center text-neutral-100 text-shadow-lg">
+          <h4 class="text-2xl font-bold mb-3">Individuelle Beratung</h4>
+          <p>
+            Strukturierte, klientenzentrierte Behandlungskonzepte mit
+            transparenten Therapiezielen
+          </p>
+        </div>
+        <div class="text-center text-neutral-100 text-shadow-lg">
+          <h4 class="text-2xl font-bold mb-3">Prävention & Coaching</h4>
+          <p>
+            Ergonomieberatung, Stressmanagement und betriebliche
+            Gesundheitsförderung
+          </p>
+        </div>
       </div>
     </div>
 
     <!-- decorative shapes -->
     <div
-      class="bg-primary-500 absolute -z-10 -top-10 -right-96"
+      class="bg-primary-500 absolute -z-10 -top-10"
       style="width: 2000px; height: 616px; rotate: -3deg"
     ></div>
   </section>
