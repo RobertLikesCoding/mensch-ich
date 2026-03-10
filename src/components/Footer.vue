730c2c962507c1dca519f5b024a79d9b2e65d1a5
RobertLikesCoding <rbaufeld@posteo.de>
2026-03-10 20:22:33 +0100

    create footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="flex text-neutral-100 bg-neutral-900">
+    <div class="flex">
+      <img
+        src="/src/assets/logo_menschlich.svg"
+        alt="Logo Ergotherapie Menschlich."
+        class="text-neutral-100"
+      />
+      <img
+        src="/src/assets/logo_bed.jpeg"
+        alt="Logo Bundesverband für Ergotherapeut/innen."
+        style="width: 100px"
+      />
+    </div>
+    <div class="flex flex-col">
+      <ul class="flex gap-5 md:gap-10">
+        <li class="hover:text-shadow-lg"><a href="#über">Impressum</a></li>
+        <li class="hover:text-shadow-lg"><a href="#team">Datenschutz</a></li>
+      </ul>
+    </div>
+    <p>© 2026 Mensch/ich - Praxis für Ergotherapie. Alle Rechte vorbehalten.</p>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="css" scoped></style>
