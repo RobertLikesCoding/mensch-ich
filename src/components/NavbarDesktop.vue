1bf8b4566a005cc0c7fdb878e01177a537851ee0
RobertLikesCoding <rbaufeld@posteo.de>
2026-03-15 15:42:25 +0100

    replace .content-box with max-w-5xl and add hover effect to navbar items

--- src/components/NavbarDesktop.vue
+++ src/components/NavbarDesktop.vue
@@ -5,12 +5,18 @@
       alt="Logo Ergotherapie Menschlich."
     />
     <ul class="flex gap-5 md:gap-10">
-      <li class="hover:text-shadow-lg">
-        <a href="#leistungen">Leistungen</a>
+      <li class="hover:underline underline-offset-5">
+        <a href="/#leistungen">Leistungen</a>
       </li>
-      <li class="hover:text-shadow-lg"><a href="#über">Über uns</a></li>
-      <li class="hover:text-shadow-lg"><a href="#team">Team</a></li>
-      <li class="hover:text-shadow-lg"><a href="#kontakt">Kontakt</a></li>
+      <li class="hover:underline underline-offset-5">
+        <a href="/#überuns">Über uns</a>
+      </li>
+      <li class="hover:underline underline-offset-5">
+        <a href="/#team">Team</a>
+      </li>
+      <li class="hover:underline underline-offset-5">
+        <a href="/#kontakt">Kontakt</a>
+      </li>
     </ul>
   </nav>
 </template>
