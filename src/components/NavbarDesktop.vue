af69a01f9432a013985742939da024bd8d4b679e
RobertLikesCoding <rbaufeld@posteo.de>
2026-04-19 21:52:53 +0200

    basic setup for a language switcher

--- src/components/NavbarDesktop.vue
+++ src/components/NavbarDesktop.vue
@@ -1,20 +1,32 @@
 <template>
   <nav>
     <a href="/#page-top" class="flex-1">
-      <img :src="imgLogo.src" alt="Logo der Ergotherapie Mensch/ich." />
+      <img :src="imgLogo.src" :alt="t('nav.logo_alt')" />
     </a>
     <ul class="flex gap-5 md:gap-10 items-center justify-evenly flex-2">
       <li class="hover:underline underline-offset-5">
-        <a href="/#leistungen">Leistungen</a>
+        <a href="/#leistungen">{{ t("nav.services") }}</a>
       </li>
       <li class="hover:underline underline-offset-5">
-        <a href="/#überuns">Über uns</a>
+        <a href="/#überuns">{{ t("nav.about_us") }}</a>
       </li>
       <li class="hover:underline underline-offset-5">
-        <a href="/#team">Team</a>
+        <a href="/#team">{{ t("nav.team") }}</a>
+      </li>
+      <li class="flex items-center gap-2">
+        <div class="flex">
+          <a href="/"> DE </a>
+          /
+          <a href="/en"> EN </a>
+        </div>
+        <LanguageIcon />
       </li>
       <li>
-        <Button label="Kontakt" to="/#kontakt" style="height: 30px" />
+        <Button
+          :label="t('nav.button.contact')"
+          to="/#kontakt"
+          style="height: 30px"
+        />
       </li>
     </ul>
   </nav>
@@ -23,4 +35,9 @@
 <script setup lang="ts">
   import imgLogo from "../assets/logo_menschlich.svg";
   import Button from "./UI/Button.vue";
+
+  import { useTranslations } from "../i18n/utils";
+  import LanguageIcon from "./UI/LanguageIcon.vue";
+
+  const t = useTranslations();
 </script>
---
+++ src/i18n/locale.ts
@@ -0,0 +1,21 @@
+export const languages = {
+  de: "Deutsch",
+  en: "English",
+};
+
+export const locale = {
+  de: {
+    "nav.services": "Leistungen",
+    "nav.about_us": "Über uns",
+    "nav.team": "Team",
+    "nav.logo_alt": "Logo der Ergotherapie Mensch/ich.",
+    "nav.button.contact": "Kontakt",
+  },
+  en: {
+    "nav.services": "Services",
+    "nav.about_us": "About us",
+    "nav.team": "Team",
+    "nav.logo_alt": "Logo of the practice Menschlich",
+    "nav.button.contact": "Contact",
+  },
+} as const;
