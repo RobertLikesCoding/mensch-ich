ec858ed3e1c47f5c6818b84431508f0df3d3d398
RobertLikesCoding <rbaufeld@posteo.de>
2026-08-19 22:43:01 +0200

    optimize images and svgs and preload fonts

--- src/components/NavbarMobile.vue
+++ src/components/NavbarMobile.vue
@@ -7,9 +7,11 @@
     >
       <a :href="buildLocalePath(lang, '#page-top')">
         <img
-          :src="imgLogo.src"
+          :src="logoSvg.src"
           alt="Logo der Ergotherapie Mensch/ich."
           class="h-20 ps-3"
+          width="193"
+          height="80"
         />
       </a>
 
@@ -73,7 +75,7 @@
 
 <script setup lang="ts">
   import { ref } from "vue";
-  import imgLogo from "../assets/logo_menschlich.svg";
+  import logoSvg from "../assets/logo_menschlich.svg";
   import { useTranslations, buildLocalePath } from "../i18n/languageUtils";
   import LanguageSwitcher from "./LanguageSwitcher.vue";
   import type { SupportedLanguages } from "../i18n/locale.ts";
